5f27b02b61b6bec77479c5324ff271e21260a929
Kritsadapruet <kritsadapruet2018@gmail.com>
2023-07-12 10:15:51 +0700

    05/07/66

--- pages/api/users/[...id].js
+++ pages/api/users/[...id].js
@@ -2,14 +2,15 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'db_user'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
   });
   export default function handler(req, res) {
     const { id } = req.query
   connection.query(
-    'SELECT * FROM `tbl_users` WHERE `id` = ? ',[id],
+    'SELECT * FROM tbl_users WHERE id = ? ',[id],
     function(err, results) {
       console.log(results); // results contains rows returned by server
       res.status(200).json({users: results});
