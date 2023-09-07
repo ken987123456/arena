bdd5fecd3801202e4539b64ca0b6d11fcbe22b7e
Kritsadapruet <kritsadapruet2018@gmail.com>
2023-09-07 13:24:39 +0700

    07/09/66-1

--- pages/api/users/[...id].js
+++ pages/api/users/[...id].js
@@ -10,6 +10,16 @@ const connection = mysql.createConnection({
   });
   export default function handler(req, res) {
     const { id } = req.query
+    if (req.method === "DELETE"){
+      try{
+        const result = connection.query("DELETE FROM tbl_users WHERE id = ?", [req.query.id ]);
+         return res.status(200).json({...req.body, id: result.insertld});
+        } catch(error) {
+          return res.status(500).json({message: error.message});
+        }
+    } else {
+
+    }
   connection.query(
     'SELECT * FROM tbl_users WHERE id = ? ',[id],
     function(err, results) {
--- pages/dashboard/user/edit/[id].js
+++ pages/dashboard/user/edit/[id].js
@@ -3,8 +3,8 @@ import { useRouter } from "next/router";
 import Link from 'next/link';
 
 export async function getServerSideProps(req) {
-    const id = req.query;
-    const res = await fetch('https://frontend-ib7j.vercel.app/api/users?id=' + id, {
+    const { id } = req.query
+    const res = await fetch('https://frontend-ib7j.vercel.app/api/users/' + id, {
       method: 'GET',
     })
     const posts = await res.json();
